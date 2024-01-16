562d1de5aefeae44eabf1c0eb9bd6dccbbaa835d
Patrik Mintěl <ja@patrick115.eu>
2024-01-16 21:09:04 +0100

    Added ono and updated owo

--- src/commands/ono.ts
+++ src/commands/ono.ts
@@ -2,8 +2,8 @@ import { Event } from '../loader'
 
 export const events: Event<any>[] = [
     new Event('message', async (msg) => {
-        if (msg.message.toLocaleLowerCase().includes('uwu') && !msg.user.isBot()) {
-            msg.reply('owo')
+        if (msg.message.toLocaleLowerCase().includes('owo') && !msg.user.isBot()) {
+            msg.reply('OnO')
         }
     }),
 ]
