1c06b0802c8ed9a290f1d2ccb9e71a1c1752a38c
Patrik Mintěl <ja@patrick115.eu>
2024-01-17 22:01:37 +0100

    Added baf command

---
+++ src/commands/baf.ts
@@ -0,0 +1,9 @@
+import { Event } from '../loader'
+
+export const events: Event<any>[] = [
+    new Event('message', async (msg) => {
+        if (msg.message.toLocaleLowerCase() == 'baf' && !msg.user.isBot()) {
+            msg.reply('lek AHHHHHH')
+        }
+    }),
+]
