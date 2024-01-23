2a9dbb0388a60b49e1dbb77ab48976e95d8b6bc5
Patrik Mintěl <ja@patrick115.eu>
2024-01-23 21:03:57 +0100

    Added bla command

---
+++ src/commands/bla.ts
@@ -0,0 +1,9 @@
+import { Event } from '../loader'
+
+export const events: Event<any>[] = [
+    new Event('message', async (msg) => {
+        if (msg.message.toLocaleLowerCase().includes('bla') && !msg.user.isBot()) {
+            msg.reply('bla')
+        }
+    }),
+]
