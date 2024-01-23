2517146d24c019510b994001ecb53c9872f3550a
Patrik Mintěl <ja@patrick115.eu>
2024-01-23 21:28:43 +0100

    Rewrite logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -24,10 +24,12 @@ class Logger {
     name: string
     color: color
     time: number = 0
+    private sensitive: boolean
 
-    constructor(name: string, color: color = 'yellow') {
+    constructor(name: string, color: color = 'yellow', sensitive = false) {
         this.name = name
         this.color = color
+        this.sensitive = sensitive
     }
 
     getTime() {
@@ -48,94 +50,62 @@ class Logger {
     }
 
     log(message: any) {
-        if (typeof message == 'object') {
-            message = JSON.stringify(message, null, 4)
-        }
-
-        this.logToFile(
-            `${clc.white('[')}${clc.green(this.getTime())}${clc.white(']')} ${clc.white('[')}${clc.blue(
-                'INFO',
-            )}${clc.white(']')} ${clc.white('[')}${clc[this.color](this.name)}${clc.white(']')} ${message}`,
-        )
+        this.logToFile('INFO', message)
     }
 
     start(message: any) {
-        if (typeof message == 'object') {
-            message = JSON.stringify(message, null, 4)
-        }
-
         this.time = Date.now()
-        this.logToFile(
-            `${clc.white('[')}${clc.green(this.getTime())}${clc.white(']')} ${clc.white('[')}${clc.blue(
-                'INFO',
-            )}${clc.white(']')} ${clc.white('[')}${clc[this.color](this.name)}${clc.white(']')} ${message}`,
-        )
+        this.logToFile('INFO', message)
     }
 
     stop(message: any) {
-        if (typeof message == 'object') {
-            message = JSON.stringify(message, null, 4)
-        }
-
         let ms = Date.now() - this.time
         this.time = 0
 
-        this.logToFile(
-            `${clc.white('[')}${clc.green(this.getTime())}${clc.white(']')} ${clc.white('[')}${clc.blue(
-                'INFO',
-            )}${clc.white(']')} ${clc.white('[')}${clc[this.color](this.name)}${clc.white(']')} ${message} ${clc.white(
-                `(${ms} ms)`,
-            )}`,
-        )
+        this.logToFile('INFO', message, ms)
     }
 
     stopError(message: any) {
-        if (typeof message == 'object') {
-            message = JSON.stringify(message, null, 4)
-        }
-
         let ms = Date.now() - this.time
         this.time = 0
 
-        this.logToFile(
-            `${clc.white('[')}${clc.green(this.getTime())}${clc.white(']')} ${clc.white('[')}${clc.red(
-                'ERROR',
-            )}${clc.white(']')} ${clc.white('[')}${clc[this.color](this.name)}${clc.white(']')} ${clc.red(
-                message,
-            )} ${clc.white(`(${ms} ms)`)}`,
-        )
+        this.logToFile('ERROR', message, ms)
     }
 
     error(message: any) {
-        if (typeof message == 'object') {
-            message = JSON.stringify(message, null, 4)
-        }
-
-        this.logToFile(
-            `${clc.white('[')}${clc.green(this.getTime())}${clc.white(']')} ${clc.white('[')}${clc.red(
-                'ERROR',
-            )}${clc.white(']')} ${clc.white('[')}${clc[this.color](this.name)}${clc.white(']')} ${clc.red(message)}`,
-        )
+        this.logToFile('ERROR', message)
     }
 
-    logToFile(formattedMessage: string) {
-        //strip formatting of message and save it to file named by current date YYYY-MM-DD.log if date is only one digit add 0 in front and at the end console log the formatted message
-        let date = new Date()
-        let year = date.getFullYear()
-        let month = date.getMonth() + 1
-        let day = date.getDate()
+    logToFile(type: 'INFO' | 'ERROR', data: any, ms?: number) {
+        const textType = type == 'INFO' ? clc.blue('INFO') : clc.red('ERROR')
+        const textColor = type == 'INFO' ? clc.white : clc.red
 
-        //filename
-        let filename = `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}.log`
+        if (this.sensitive) {
+            console.log(
+                `${clc.white('[')}${clc.green(this.getTime())}${clc.white(']')} ${clc.white('[')}${textType}${clc.white(']')} ${clc.white('[')}${clc[this.color](this.name)}${clc.white(']')}`,
+                data,
+                ms ? `${clc.white(`(${ms} ms)`)}` : '',
+            )
+        } else {
+            const formattedMessage = `${clc.white('[')}${clc.green(this.getTime())}${clc.white(']')} ${clc.white('[')}${textType}${clc.white(']')} ${clc.white('[')}${clc[this.color](this.name)}${clc.white(']')} ${textColor(data)} ${ms ? `${clc.white(`(${ms} ms)`)}` : ''}`
 
-        //strip formatting
-        let message = strip(formattedMessage)
+            console.log(formattedMessage)
 
-        //add it to new line of file
-        fs.appendFileSync(`./logs/${filename}`, message + '\n')
+            //strip formatting of message and save it to file named by current date YYYY-MM-DD.log if date is only one digit add 0 in front and at the end console log the formatted message
+            let date = new Date()
+            let year = date.getFullYear()
+            let month = date.getMonth() + 1
+            let day = date.getDate()
 
-        //log formatted message
-        console.log(formattedMessage)
+            //filename
+            let filename = `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}.log`
+
+            //strip formatting
+            let message = strip(formattedMessage)
+
+            //add it to new line of file
+            fs.appendFileSync(`./logs/${filename}`, message + '\n')
+        }
     }
 }
 
