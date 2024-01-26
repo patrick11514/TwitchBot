5b976ada9c74e4190d92f922b979ee439c66bb89
Patrik Mintěl <ja@patrick115.eu>
2024-01-26 22:10:44 +0100

    Add and remove bots

--- src/types/database.ts
+++ src/types/database.ts
@@ -5,7 +5,7 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   : ColumnType<T, T | undefined, T>;
 
 export interface Bots {
-  id: number;
+  id: string;
   username: string;
 }
 
