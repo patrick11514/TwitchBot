c3fe63c45632d01d1c15af776e0c0ebd1c72974e
Patrik Mintěl <ja@patrick115.eu>
2024-01-26 21:27:39 +0100

    Change method to property

--- src/commands/baf.ts
+++ src/commands/baf.ts
@@ -2,7 +2,7 @@ import { Event } from '../loader'
 
 export const events: Event<any>[] = [
     new Event('message', async (msg) => {
-        if (msg.message.toLocaleLowerCase() == 'baf' && !msg.user.isBot()) {
+        if (msg.message.toLocaleLowerCase() == 'baf' && !msg.user.isBot) {
             msg.reply('lek AHHHHHH')
         }
     }),
--- src/commands/owo.ts
+++ src/commands/owo.ts
@@ -2,7 +2,7 @@ import { Event } from '../loader'
 
 export const events: Event<any>[] = [
     new Event('message', async (msg) => {
-        if (msg.message.toLocaleLowerCase().includes('uwu') && !msg.user.isBot()) {
+        if (msg.message.toLocaleLowerCase().includes('uwu') && !msg.user.isBot) {
             msg.reply('owo')
         }
     }),
