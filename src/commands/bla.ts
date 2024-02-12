71007cf82610de08528f6b53944197abf0bc9314
Patrik Mintěl <ja@patrick115.eu>
2024-02-12 22:16:38 +0100

    Fixed bla

--- src/commands/bla.ts
+++ src/commands/bla.ts
@@ -2,7 +2,21 @@ import { Event } from '../loader'
 
 export const events: Event<any>[] = [
     new Event('message', async (msg) => {
-        if (msg.message.toLocaleLowerCase().includes('bla') && !msg.user.isBot) {
+        const message = msg.message.toLocaleLowerCase()
+
+        let includes = false
+
+        if (message == 'bla') {
+            includes = true
+        }
+
+        if (message.includes('bla')) {
+            if (message.includes(' bla') || message.includes('bla ')) {
+                includes = true
+            }
+        }
+
+        if (includes && !msg.user.isBot) {
             msg.reply('bla')
         }
     }),
